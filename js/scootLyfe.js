265802c8e3b03bd32a725c7a6ec341c0e41a059c
Matt Wujek <mattwujek@gmail.com>
2016-04-18 05:29:44 -0700

    saving changes and going to bed.

--- js/scootLyfe.js
+++ js/scootLyfe.js
@@ -6,9 +6,12 @@ $(document).ready(function() {
 $('.how-to-slider').flickity({
   // options
   prevNextButtons: false,
-  //pageDots: false,
+  draggable: false,
   cellSelector: '.cell'
 });
+
+
+
 
 
 var nextBtn = $('.next-btn');
@@ -23,11 +26,47 @@ nextBtn.on( 'click', function() {
   $('.how-to-slider').flickity('next');
 });
 
-$('.how-to-slider').flickity('select', 1);
+$('.how-to-slider').flickity('select', 6);
 
 }); // end of ready
 
+$('.reveal-section').each(function() {
+	var imgWrapper = $(this).find('.image-wrapper');
+	imgWrapper.addClass('hidden');
+});
 
+$('.image-reveal-btn').click(function() {
+
+	var wrap = $(this).parent().find('.image-wrapper')
+	var thisBtn = $(this);
+	if (wrap.hasClass('hidden') ){
+		wrap.removeClass('hidden');
+		thisBtn.text('Hide Demo');
+		thisBtn.addClass('active-btn');
+	} else {
+		wrap.addClass('hidden');
+		thisBtn.text('View Demo');
+		thisBtn.removeClass('active-btn');
+	}
+	
+});
+
+var startBtn = $('.get-started-btn');
+
+
+var flkty = Flickity.data( $('.how-to-slider') )
+$('.how-to-slider').on( 'settle', function() {
+    var page = $(this).data().flickity.selectedIndex;
+    console.log(page);
+    if (page === 7){
+    	startBtn.addClass('get-started-now');
+    	console.log('go!');
+    }
+});
+
+startBtn.click(function() {
+	window.alert('User can now start using the app :-)')
+});
 
 
 // $(window).resize(function() {
